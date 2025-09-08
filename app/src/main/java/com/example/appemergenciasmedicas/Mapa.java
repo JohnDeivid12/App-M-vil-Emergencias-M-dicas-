60b084827b0f5c9fbacc6b90b99c53c32f628517
JohnDeivid <johndeivid202@gmail.com>
2025-09-08 15:10:08 -0500

    Version 1: Solo pantallas (Activities)

---
+++ app/src/main/java/com/example/appemergenciasmedicas/Mapa.java
@@ -0,0 +1,18 @@
+package com.example.appemergenciasmedicas;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Mapa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapa);
+    }
+}
